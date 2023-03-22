c796dcf2b7586691bea34c8bb6a01fdc048e4c42
Muhammad Mustafa <mustafa9140@gmail.com>
2023-03-22 15:36:25 +0500

    Initial commit

---
+++ backend/config/allowedOrigin.js
@@ -0,0 +1,8 @@
+const allowedOrigin = [
+    'http://localhost:3000',
+    'https://www.dandrepairshop.com',
+    'https://dandrepairshop.com',
+    'https://www.google.com/'
+]
+
+module.exports = allowedOrigin
